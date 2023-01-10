d7b9d3fe18342ed5b65ff4e10a02a2b25337c934
MartinezEmmanuel03 <e.martinez03@reseau.sncf.fr>
2023-01-10 17:39:33 +0100

    mise en place pages nav footer et navigation

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,26 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Footer from "@components/Footer/Footer";
+import Nav from "@components/Nav/Nav";
+import Page1 from "@pages/Page1";
+import Page2 from "@pages/Page2";
+import Page3 from "@pages/Page3";
+import Page4 from "@pages/Page4";
 import Home from "./pages/Home";
-
 import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <Home />
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
-    </div>
+    <BrowserRouter>
+      <Nav />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/page1" element={<Page1 />} />
+        <Route path="/page2" element={<Page2 />} />
+        <Route path="/page3" element={<Page3 />} />
+        <Route path="/page4" element={<Page4 />} />
+      </Routes>
+      <Footer />
+    </BrowserRouter>
   );
 }
 
