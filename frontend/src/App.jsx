ded67ff4d1f08f9c5b4c7828c0c87cec96c7144b
MartinezEmmanuel03 <e.martinez03@reseau.sncf.fr>
2023-01-11 21:37:50 +0100

    ajout d'un layout pour la partie publique

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,34 +1,32 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
-import Nav from "@components/Nav/Nav";
 import Page1 from "@pages/Page1";
 import Page2 from "@pages/Page2";
+import Page3 from "@pages/Page3";
 import Dashboard from "@pages/Layout/Dashboard";
+import PublicLayout from "@pages/Layout/PublicLayout";
 import Information from "@pages/Back-office/Information";
 import Commentary from "@pages/Back-office/Commentary";
 import Rental from "@pages/Back-office/Rental";
-import OneVehicule from "@pages/OneVehicule";
-import Footer from "@components/Footer/Footer";
-
 import Home from "./pages/Home";
 import "./App.css";
 
 function App() {
   return (
     <Router>
-      <Nav />
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/page1" element={<Page1 />} />
-        <Route path="/page2" element={<Page2 />} />
-        <Route path="/vehicule" element={<OneVehicule />} />
+        <Route path="/" element={<PublicLayout />}>
+          <Route path="/" element={<Home />} />
+          <Route path="page1" element={<Page1 />} />
+          <Route path="page2" element={<Page2 />} />
+          <Route path="page3" element={<Page3 />} />
+        </Route>
         <Route path="/Dashboard" element={<Dashboard />}>
           <Route path="Information" element={<Information />} />
           <Route path="Commentary" element={<Commentary />} />
           <Route path="Rental" element={<Rental />} />
         </Route>
       </Routes>
-      <Footer />
     </Router>
   );
 }
