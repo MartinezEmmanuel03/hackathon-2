5aad7571dd3d92b41922cf8ae8cae8cee483eea2
guillaume wernert <guil.wernert@gmail.com>
2023-01-11 19:56:58 +0100

    ajout route getById vehicule

--- backend/src/router.js
+++ backend/src/router.js
@@ -1,5 +1,8 @@
 const express = require("express");
+const VehiculeController = require("./controllers/VehiculeController");
 
 const router = express.Router();
+
+router.get("/vehicule/:id", VehiculeController.read);
 
 module.exports = router;
