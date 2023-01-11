e47bbef6ebf17a8fe7860b9432b9f3f0976b389d
PETACCIA <petaccia.seb@gmail.com>
2023-01-11 21:17:39 +0100

    modification de la taille de l'image de la page home

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -3,7 +3,7 @@ import img1 from "../assets/kitt2.jpg";
 export default function Home() {
   return (
     <div>
-      <img className="" alt="" src={img1} />
+      <img className="w-full" alt="" src={img1} />
     </div>
   );
 }
