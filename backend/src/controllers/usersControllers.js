665953fa09707bb68cebcb60b66ecc93ad03d429
vassili45 <vassili.papaodpoulos45@gmail.com>
2023-01-11 19:40:41 +0100

    add put and add

--- backend/src/controllers/usersControllers.js
+++ backend/src/controllers/usersControllers.js
@@ -27,7 +27,43 @@ const read = (req, res) => {
       res.sendStatus(500);
     });
 };
+const edit = (req, res) => {
+  const users = req.body;
+
+  users.id = parseInt(req.params.id, 10);
+
+  models.users
+    .update(users)
+    .then(([result]) => {
+      if (result.affectedRows === 0) {
+        res.sendStatus(404);
+      } else {
+        res.sendStatus(204);
+      }
+    })
+    .catch((err) => {
+      console.error(err);
+      res.sendStatus(500);
+    });
+};
+
+const add = (req, res) => {
+  const users = req.body;
+
+  models.users
+    .insert(users)
+    .then(([result]) => {
+      res.location(`/users/${result.insertId}`).sendStatus(201);
+    })
+    .catch((err) => {
+      console.error(err);
+      res.sendStatus(500);
+    });
+};
+
 module.exports = {
   browse,
   read,
+  edit,
+  add,
 };
--- backend/src/router.js
+++ backend/src/router.js
@@ -6,5 +6,7 @@ const usersControllers = require("./controllers/usersControllers");
 
 router.get("/users", usersControllers.browse);
 router.get("/users/:id", usersControllers.read);
+router.put("/users/:id", usersControllers.edit);
+router.post("/users", usersControllers.add);
 
 module.exports = router;
