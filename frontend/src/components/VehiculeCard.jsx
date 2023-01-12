b3b700c259a9f70a4ba0a02ec8d79bf6bcb7df78
MartinezEmmanuel03 <e.martinez03@reseau.sncf.fr>
2023-01-12 15:07:37 +0100

    fix du lien des images dans la partie front du composant VehiculeCard

--- frontend/src/components/VehiculeCard.jsx
+++ frontend/src/components/VehiculeCard.jsx
@@ -4,19 +4,13 @@ import { Link } from "react-router-dom";
 function VehiculeCard({ vehicule }) {
   const { id, photo, nameV, film } = vehicule;
   return (
-    <div className="w-5/6 md:w-3/12 border mx-auto md:mx-none border-black rounded-lg flex flex-col items-center">
-      <h1 className="text-2xl font-bold text-center">{nameV}</h1>
-      <h2>{film}</h2>
-      <img src={photo} alt="" />
-      <Link to={`/vehicules/${id}`}>
-        <button
-          type="button"
-          className="text-lg font-bold bg-black text-white w-full "
-        >
-          Voir plus
-        </button>
-      </Link>
-    </div>
+    <Link to={`/vehicules/${id}`}>
+      <div className="w-5/6 md:w-3/12 border mx-auto md:mx-none border-black rounded-lg flex flex-col items-center">
+        <h1 className="text-2xl font-bold text-center">{nameV}</h1>
+        <h2>{film}</h2>
+        <img src={`${import.meta.env.VITE_BACKEND_URL}/${photo}`} alt="" />
+      </div>
+    </Link>
   );
 }
 
