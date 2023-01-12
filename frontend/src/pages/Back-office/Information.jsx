11c83429bcf428ac8103b61658ef8a1e4a1db68d
vassili45 <vassili.papaodpoulos45@gmail.com>
2023-01-12 22:04:55 +0100

    hotfix

--- frontend/src/pages/Back-office/Information.jsx
+++ frontend/src/pages/Back-office/Information.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 import instance from "@services/apiConnexion";
-import { useParams } from "react-router-dom";
+// import { useParams } from "react-router-dom";
 
 import ConnectForm from "@components/ConnectForm";
 import ButtonTemplate from "@components/ButtonTemplate";
