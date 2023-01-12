bae822ccbc20cbed72533ebc0ecd2f36903601bc
guillaume wernert <guil.wernert@gmail.com>
2023-01-12 16:07:50 +0100

    refacto page catalogue+controllers

--- frontend/src/components/VehiculeCard.jsx
+++ frontend/src/components/VehiculeCard.jsx
@@ -5,7 +5,7 @@ function VehiculeCard({ vehicule }) {
   const { id, photo, nameV, film } = vehicule;
   return (
     <Link to={`/vehicules/${id}`}>
-      <div className="w-5/6 md:w-3/12 border mx-auto md:mx-none border-black rounded-lg flex flex-col items-center">
+      <div className=" mb-5 w-5/6 md:w-3/12 border mx-auto md:mx-none border-black rounded-lg flex flex-col items-center">
         <h1 className="text-2xl font-bold text-center">{nameV}</h1>
         <h2>{film}</h2>
         <img src={`${import.meta.env.VITE_BACKEND_URL}/${photo}`} alt="" />
