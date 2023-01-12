52ca3efab89abae93000fb2b277c5b9be096e270
guillaume wernert <guil.wernert@gmail.com>
2023-01-12 02:03:58 +0100

    page addvehicule en dur version mobile

--- backend/src/router.js
+++ backend/src/router.js
@@ -1,17 +1,14 @@
 const express = require("express");
 const VehiculeController = require("./controllers/VehiculeController");
 const usersControllers = require("./controllers/usersControllers");
+
 const router = express.Router();
 
-
 router.get("/vehicule/:id", VehiculeController.read);
-
-
 
 router.get("/users", usersControllers.browse);
 router.get("/users/:id", usersControllers.read);
 router.put("/users/:id", usersControllers.edit);
 router.post("/users", usersControllers.add);
-
 
 module.exports = router;
