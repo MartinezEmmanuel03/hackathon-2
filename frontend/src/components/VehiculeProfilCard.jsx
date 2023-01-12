ca75a6b54da1b39169ca9ede4c36c850bde6429c
PETACCIA <petaccia.seb@gmail.com>
2023-01-13 00:18:27 +0100

    mise en page de la page profil avec les cards et le formulaire

--- frontend/src/components/VehiculeProfilCard.jsx
+++ frontend/src/components/VehiculeProfilCard.jsx
@@ -1,22 +1,23 @@
 import React from "react";
-import img1 from "../assets/jurassic.png";
 
-function VehiculeProfilCard() {
+function VehiculeProfilCard({ vehicule }) {
+  const { photo, nameV } = vehicule;
+
   return (
-    <div className="flex justify-center">
+    <div className="flex justify-center md:mb-5">
       <div className="flex flex-col md:flex-row md:max-w-xl rounded-lg bg-white shadow-lg md:justify-center md:flex-wrap">
         <img
-          className=" w-full h-full md:h-auto object-cover md:w-48 rounded-t-lg md:rounded-none md:rounded-l-lg"
-          src={img1}
-          alt=""
+          className=" w-full h-full md:h-auto object-cover md:w-48 rounded-t-lg md:rounded "
+          src={`${import.meta.env.VITE_BACKEND_URL}/${photo}`}
+          alt="car"
         />
         <div className="p-6 flex flex-col justify-start">
           <p className="text-gray-700 text-base mb-4">
-            <h5 className="text-gray-900 text-xl font-medium mb-2">
-              Nom de la voiture :
-            </h5>
-            This is a wider card with supporting text below as a natural lead-in
-            to additional content. This content is a little bit longer.
+            <h5 className="text-gray-900 text-xl font-medium mb-2">{nameV}</h5>
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit.Rerum
+            voluptatem vero ut molestias expedita temporibus, qui distinctio,
+            laudantium iusto culpa cumque. Expedita omnis dicta consectetur
+            eveniet blanditiis, voluptatem atque nisi?
             <textarea
               id="commentaire"
               type=""
@@ -26,6 +27,12 @@ function VehiculeProfilCard() {
             />
           </p>
           <p className="text-gray-600 text-xs" />
+          <button
+            type="button"
+            className="rounded-md border-r w-32 m-auto bg-gray-500  hover:bg-gray-800 text-white font-bold py-2 px-4 border-b-4 "
+          >
+            envoyer
+          </button>
         </div>
       </div>
     </div>
--- frontend/src/pages/Profil.jsx
+++ frontend/src/pages/Profil.jsx
@@ -1,7 +1,17 @@
 import VehiculeProfilCard from "@components/VehiculeProfilCard";
-import React from "react";
+import instance from "@services/apiConnexion";
+import React, { useEffect, useState } from "react";
 
 function Profil() {
+  const [vehicule, setVehicules] = useState([]);
+
+  useEffect(() => {
+    instance
+      .get("./vehicule/random")
+      .then((res) => setVehicules(res.data))
+      .catch((err) => console.error(err));
+  }, []);
+
   return (
     <div className="pt-12 md:flex md:w-full md:content-center">
       <section className=" m-3 mr-3 p-6 md:m-8 md:mt-10 md:mb-5  bg-indigo-600 rounded-md shadow-md dark:bg-gray-800 md:w-1/3 md:h-11/12 md:my-0">
@@ -18,7 +28,7 @@ function Profil() {
                 id="nom"
                 type="text"
                 required
-                className="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-300 rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-500 dark:focus:border-blue-500 focus:outline-none focus:ring"
+                className="block w-full px-4 py-2 mt-2 focus:outline-none focus:ring"
               />
             </div>
 
@@ -30,22 +40,7 @@ function Profil() {
                 id="prenom"
                 type="text"
                 required
-                className="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-300 rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-500 dark:focus:border-blue-500 focus:outline-none focus:ring"
-              />
-            </div>
-
-            <div>
-              <label
-                className="text-white dark:text-gray-200"
-                htmlFor="adresse"
-              >
-                Adresse
-              </label>
-              <input
-                id="adresse"
-                type="text"
-                required
-                className="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-300 rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-500 dark:focus:border-blue-500 focus:outline-none focus:ring"
+                className="block w-full px-4 py-2 mt-2  focus:outline-none focus:ring"
               />
             </div>
 
@@ -57,7 +52,47 @@ function Profil() {
                 id="age"
                 type="date"
                 required
-                className="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-300 rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-500 dark:focus:border-blue-500 focus:outline-none focus:ring"
+                className="block  w-full px-4 py-2 mt-2  focus:outline-none focus:ring"
+              />
+            </div>
+            <div>
+              <label
+                className="text-white dark:text-gray-200"
+                htmlFor="adresse"
+              >
+                Adresse
+              </label>
+              <input
+                id="adresse"
+                type="text"
+                required
+                className="block w-full px-4 py-2 mt-2  focus:outline-none focus:ring"
+              />
+            </div>
+            <div>
+              <label
+                className="text-white dark:text-gray-200"
+                htmlFor="code-postal"
+              >
+                Code postal
+              </label>
+              <input
+                id="code-postal"
+                type="number"
+                required
+                className="block w-full px-4 py-2 mt-2  focus:outline-none focus:ring"
+              />
+            </div>
+
+            <div>
+              <label className="text-white dark:text-gray-200" htmlFor="ville">
+                Ville
+              </label>
+              <input
+                id="ville"
+                type="text"
+                required
+                className="block w-full px-4 py-2 mt-2  focus:outline-none focus:ring"
               />
             </div>
 
@@ -69,19 +104,69 @@ function Profil() {
                 id="email"
                 type="email"
                 required
-                className="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-300 rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-500 dark:focus:border-blue-500 focus:outline-none focus:ring "
+                className="block w-full px-4 py-2 mt-2  focus:outline-none focus:ring "
               />
             </div>
+            <div>
+              <label className="text-white dark:text-gray-200" htmlFor="pays">
+                pays
+              </label>
+              <input
+                id="pays"
+                type="text"
+                required
+                className="block w-full px-4 py-2 mt-2  focus:outline-none focus:ring "
+              />
+            </div>
+            {/* <div>
+              <label className="block text-sm font-medium text-white">
+                Image
+              </label>
+              <div className="mt-1 flex justify-center px-6 pt-5 pb-6 border-2 border-gray-300 border-dashed rounded-md">
+                <div className="space-y-1 text-center">
+                  <svg
+                    className="mx-auto h-12 w-12 text-white"
+                    stroke="currentColor"
+                    fill="none"
+                    viewBox="0 0 48 48"
+                    aria-hidden="true"
+                  >
+                    <path
+                      d="M28 8H12a4 4 0 00-4 4v20m32-12v8m0 0v8a4 4 0 01-4 4H12a4 4 0 01-4-4v-4m32-4l-3.172-3.172a4 4 0 00-5.656 0L28 28M8 32l9.172-9.172a4 4 0 015.656 0L28 28m0 0l4 4m4-24h8m-4-4v8m-12 4h.02"
+                      strokeWidth="2"
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                    />
+                  </svg>
+                  <div className="flex text-sm text-gray-600">
+                    <label
+                      htmlFor="file-upload"
+                      className="relative cursor-pointer bg-white rounded-md font-medium text-indigo-600 hover:text-indigo-500 focus-within:outline-none focus-within:ring-2 focus-within:ring-offset-2 focus-within:ring-indigo-500"
+                    >
+                      <span className="">Upload a file</span>
+                      <input
+                        id="file-upload"
+                        name="file-upload"
+                        type="file"
+                        className="sr-only"
+                      />
+                    </label>
+                    <p className="pl-1 text-white">or drag and drop</p>
+                  </div>
+                  <p className="text-xs text-white">PNG, JPG, GIF up to 10MB</p>
+                </div>
+              </div>
+            </div> */}
           </div>
         </form>
       </section>
-      <div className="md:flex">
-        <div className=" md:mb-5 md:mt-20 md:mr-5">
-          <VehiculeProfilCard />
+      <div className="md:flex ">
+        <div className=" md:mb-5 md:mt-20 md:ml-80">
+          {vehicule.map((vehicules) => (
+            <VehiculeProfilCard vehicule={vehicules} key={vehicules.id} />
+          ))}
         </div>
-        <div className="md:mb-5 md:mt-20">
-          <VehiculeProfilCard />
-        </div>
+        <div className="md:mb-5 md:mt-20" />
       </div>
     </div>
   );
